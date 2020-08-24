ca5d107ebd26a21897ddf47bd3261338fd7e959e
unknown <post.arslan@outlook.com>
2020-08-24 11:32:10 +0500

    :hammer: deleted extra files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, Suspense } from 'react';
+import {Container, Button, Grid, Card, CardContent, CardActions, CardActionArea, CardMedia} from '@material-ui/core'
 import './App.css';
 
 function App() {
+  let [posts, setPosts] = useState(null)
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts').then(r => r.json()).then(d => setPosts(posts = d))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fixed>
+      <Grid container m={0} spacing={2}>
+        <Suspense fallback="<p> Loading posts...</p>">
+          {  posts?.map((d, i) => (
+            <Grid item xs={4} key={i}>
+                <Card>
+                    <CardActionArea>
+                      <CardMedia className="Media" title="Card Media" image="https://upload.wikimedia.org/wikipedia/commons/c/cc/Jinnah1945c.jpg" />
+                    </CardActionArea>
+                      <CardContent>
+                        <h3>{d.title}</h3>
+                      </CardContent>
+                    <CardActions>
+                      <Button variant="contained" disableElevation>Take Action</Button>
+                      <Button variant="contained" color="primary" disableElevation>some action</Button>   
+                    </CardActions>
+                </Card>
+              </Grid>
+          ))} 
+        </Suspense>
+      </Grid>
+    </Container>
   );
 }
 
